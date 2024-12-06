9315939ed0bde882ace9450ecd294df47f2f45fa
ngothuc <ngothuc0704@gmail.com>
2024-12-07 05:41:10 +0700

    get user practice result

--- backend/src/routes/routes.c
+++ backend/src/routes/routes.c
@@ -38,6 +38,10 @@ void handle_control_message(int socket, ControlMessage *msg)
     {
         handle_get_exam_result_of_room(socket, msg);
     }
+    else if (strcmp(msg->type, GET_USER_PRACTICE_RESULT) == 0)
+    {
+        handle_get_user_practice_result(socket, msg);
+    }
 }
 
 void handle_data_message(int socket, DataMessage *msg)
--- backend/src/services/service.h
+++ backend/src/services/service.h
@@ -8,5 +8,6 @@ char *get_all_room();
 int user_enter_room(int user_id, int room_id);
 char *get_user_exam_result(int user_id, int room_id);
 char *get_exam_result_of_room(int room_id);
+char *get_user_practice_result(int room_id, int user_id);
 
 #endif // SERVICE_H
