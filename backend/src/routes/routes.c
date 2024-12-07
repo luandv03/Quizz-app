80d36f7f43b2ac1bf46af8b7595375e5ef027992
gioitrongxuan <gioi.trongxuan@gmail.com>
2024-12-07 09:31:34 +0700

    add: pthreaded socket

--- backend/src/routes/routes.c
+++ backend/src/routes/routes.c
@@ -1,13 +1,29 @@
 #include "../data_structures/index.h"
 #include "../controllers/controller.h"
+#include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define PORT 8080
 #define BACKLOG 10
+
+typedef struct pthread_arg_t {
+    int new_socket_fd;
+    struct sockaddr_in client_address;
+    /* TODO: Put arguments passed to threads here. See lines 116 and 139. */
+} pthread_arg_t;
+
+/* Thread routine to serve connection to client. */
+void *pthread_routine(void *arg);
+
+/* Signal handler to handle SIGTERM and SIGINT signals. */
+void signal_handler(int signal_number);
 
 void handle_control_message(int socket, ControlMessage *msg)
 {
@@ -71,12 +87,17 @@ void handle_notification_message(int socket, NotificationMessage *msg)
 void setup_routes(int server_fd)
 {
     struct sockaddr_in address;
-    int addrlen = sizeof(address);
     int new_socket;
+    pthread_attr_t pthread_attr;
+    pthread_arg_t *pthread_arg;
+    pthread_t pthread;
+    socklen_t client_address_len;
 
+    /* Initialise IPv4 address. */
+    memset(&address, 0, sizeof address);
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
+    address.sin_addr.s_addr = INADDR_ANY;
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
@@ -92,51 +113,112 @@ void setup_routes(int server_fd)
         exit(EXIT_FAILURE);
     }
 
+    /* Assign signal handlers to signals. */
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+        perror("signal");
+        exit(1);
+    }
+    if (signal(SIGTERM, signal_handler) == SIG_ERR) {
+        perror("signal");
+        exit(1);
+    }
+    if (signal(SIGINT, signal_handler) == SIG_ERR) {
+        perror("signal");
+        exit(1);
+    }
+
+    /* Initialise pthread attribute to create detached threads. */
+    if (pthread_attr_init(&pthread_attr) != 0) {
+        perror("pthread_attr_init");
+        exit(1);
+    }
+    if (pthread_attr_setdetachstate(&pthread_attr, PTHREAD_CREATE_DETACHED) != 0) {
+        perror("pthread_attr_setdetachstate");
+        exit(1);
+    }
+
     printf("Server listening on port %d\n", PORT);
 
     while (1)
     {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        /* Tạo tham số pthread cho mỗi kết nối đến client. */
+        pthread_arg = (pthread_arg_t *)malloc(sizeof *pthread_arg);
+        if (!pthread_arg) {
+            perror("malloc");
+            continue;
+        }
+
+        /* Chấp nhận kết nối đến client. */
+        client_address_len = sizeof pthread_arg->client_address;
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&pthread_arg->client_address, &client_address_len )) < 0)
         {
             perror("accept failed");
             close(server_fd);
             exit(EXIT_FAILURE);
         }
 
-        char buffer[2048];
-        memset(buffer, 0, sizeof(buffer));
-        read(new_socket, buffer, 2048);
+        /* Khởi tạo tham số pthread. */
+        pthread_arg->new_socket_fd = new_socket;
 
-        printf("Received:\n%s\n", buffer);
-
-        char *header = strtok(buffer, "\n");
-        char *body = header + strlen(header) + 1; // Move the pointer to the position after the newline
-
-        printf("header: %s - body: %s\n", header, body);
-
-        if (strncmp(header, "CONTROL", 7) == 0)
-        {
-            ControlMessage msg;
-            sscanf(header, "CONTROL %s", msg.type);
-            strncpy(msg.body, body, sizeof(msg.body) - 1);
-            printf("msg.type: %s\n", msg.type);
-            handle_control_message(new_socket, &msg);
+        /* Tạo luồng để phục vụ kết nối đến client. */
+        if (pthread_create(&pthread, &pthread_attr, pthread_routine, (void *)pthread_arg) != 0) {
+            perror("pthread_create");
+            free(pthread_arg);  // Giải phóng bộ nhớ nếu không thể tạo luồng
+            continue;
         }
-        else if (strncmp(header, "DATA", 4) == 0)
-        {
-            DataMessage msg;
-            sscanf(header, "DATA %s %s %d", msg.type, msg.data_type, &msg.data_size);
-            strncpy(msg.body, body, sizeof(msg.body) - 1);
-            handle_data_message(new_socket, &msg);
-        }
-        else if (strncmp(header, "NOTIFICATION", 12) == 0)
-        {
-            NotificationMessage msg;
-            sscanf(header, "NOTIFICATION %s %s", msg.type, msg.timestamp);
-            strncpy(msg.body, body, sizeof(msg.body) - 1);
-            handle_notification_message(new_socket, &msg);
-        }
-
-        close(new_socket);
+       
+        // Không đóng new_socket ở đây, để nó sẽ được đóng trong pthread_routine.
     }
+}
+
+void *pthread_routine(void *arg) {
+    pthread_arg_t *pthread_arg = (pthread_arg_t *)arg;
+    int new_socket_fd = pthread_arg->new_socket_fd;
+    struct sockaddr_in client_address = pthread_arg->client_address;
+
+    /* TODO: Put client interaction code here. */
+    char buffer[2048];
+    memset(buffer, 0, sizeof(buffer));
+    read(new_socket_fd, buffer, 2048);
+
+    printf("Received:\n%s\n", buffer);
+
+    char *header = strtok(buffer, "\n");
+    char *body = header + strlen(header) + 1; // Move the pointer to the position after the newline
+
+    printf("header: %s - body: %s\n", header, body);
+
+    if (strncmp(header, "CONTROL", 7) == 0)
+    {
+        ControlMessage msg;
+        sscanf(header, "CONTROL %s", msg.type);
+        strncpy(msg.body, body, sizeof(msg.body) - 1);
+        printf("msg.type: %s\n", msg.type);
+        handle_control_message(new_socket_fd, &msg);
+    }
+    else if (strncmp(header, "DATA", 4) == 0)
+    {
+        DataMessage msg;
+        sscanf(header, "DATA %s %s %d", msg.type, msg.data_type, &msg.data_size);
+        strncpy(msg.body, body, sizeof(msg.body) - 1);
+        handle_data_message(new_socket_fd, &msg);
+    }
+    else if (strncmp(header, "NOTIFICATION", 12) == 0)
+    {
+        NotificationMessage msg;
+        sscanf(header, "NOTIFICATION %s %s", msg.type, msg.timestamp);
+        strncpy(msg.body, body, sizeof(msg.body) - 1);
+        handle_notification_message(new_socket_fd, &msg);
+    }
+
+    close(new_socket_fd);
+
+    // Giải phóng bộ nhớ khi luồng kết thúc
+    free(pthread_arg);
+    return NULL;
+}
+
+void signal_handler(int signal_number) {
+    /* TODO: Put exit cleanup code here. */
+    exit(0);
 }
