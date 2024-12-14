f38da264b113124d6a08771efa158c5575266dc7
phund-0417 <nguyen.duc.phu@sun-asterisk.com>
2024-12-14 17:13:06 +0700

    create all migration

--- backend/src/migrations/create_question_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS question (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    difficulty INT NOT NULL,
-    content VARCHAR(255),
-    room_id INT NOT NULL,
-    FOREIGN KEY (room_id) REFERENCES room(id)
-);
--- backend/src/migrations/trigger.sql
+++ backend/src/migrations/trigger.sql
@@ -19,39 +19,3 @@ BEGIN
         WHERE id = NEW.exam_id;
     END IF;
 END
-
-
-CREATE DEFINER=`quizz`@`localhost` 
-TRIGGER `update_exam_score_after_update` 
-AFTER UPDATE ON `exam_question` 
-FOR EACH ROW 
-BEGIN
-    DECLARE old_answer INT;
-    DECLARE new_answer INT;
-    DECLARE correct_answer INT;
-
-    -- Lấy câu trả lời đúng
-    SELECT is_true 
-    INTO correct_answer
-    FROM answer_of_question
-    WHERE question_id = NEW.question_id;
-
-    -- Kiểm tra và cập nhật điểm số
-    IF OLD.user_answer = correct_answer THEN
-        -- Nếu đáp án cũ đúng
-        IF NEW.user_answer <> correct_answer THEN
-            -- Đáp án mới sai => Trừ điểm
-            UPDATE exam
-            SET score = score - 1
-            WHERE id = NEW.exam_id;
-        END IF;
-    ELSE
-        -- Nếu đáp án cũ sai
-        IF NEW.user_answer = correct_answer THEN
-            -- Đáp án mới đúng => Cộng điểm
-            UPDATE exam
-            SET score = score + 1
-            WHERE id = NEW.exam_id;
-        END IF;
-    END IF;
-END
